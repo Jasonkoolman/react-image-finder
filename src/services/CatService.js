62e4f346e56a5d1bb821a0fd29bca2cca00a9514
Jason Koolman <jason.koolman@hotmail.com>
2020-03-09 22:06:48 +0100

    feat: Implement findCatByBreed method

--- src/services/CatService.js
+++ src/services/CatService.js
@@ -1,15 +1,32 @@
 const API_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = '129bf7b6-c59d-4d63-b617-3c084c2a083d';
 
 /**
  * Find a cat image using the given criteria.
  *
  * @param  {String} query
- * @return {Promise<{url: (string|null)}>}
+ * @return {Promise<string|null>}
  */
 export function findImage(query) {
   const url = `${API_URL}/images/search?q=${query}`;
 
   return fetch(url)
+      .then(res => res.json())
+      .then(res => res.length ? res[0].url : null)
+}
+
+
+/**
+ * Find a cat by the given breed.
+ *
+ * @param  {String} query
+ * @return {Promise<string|null>}
+ */
+export function findCatByBreed(query) {
+  const url = `${API_URL}/breeds/search?q=${query}`;
+  const headers = { 'x-api-key': API_KEY };
+
+  return fetch(url, { headers })
       .then(res => res.json())
       .then(res => res.length ? res[0].url : null)
 }
